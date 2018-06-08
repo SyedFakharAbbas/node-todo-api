b7cc36b1a38fed9c777e5eb3744bfd73c989828f
Syed Fakhar Abbas <syed@shadocms.com>
2018-06-08 15:22:16 +1200

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -49,6 +49,26 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'asd123');
+    } catch (e) {
+        // return new Promise((resolve, reject) => {
+        //     reject()
+        // })
+        return Promise.reject({'error': 'Inavlid Token'});
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+}
+
 var User = mongoose.model('User', UserSchema );
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {ObjectID} = require('mongodb');
 var {moongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -116,6 +117,10 @@ app.post('/users', (req, res) => {
     //     res.status(400).send(e);
     // })
     // console.log(req.body);
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
